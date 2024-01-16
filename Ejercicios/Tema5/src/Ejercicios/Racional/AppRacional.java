0061f02584356ab216fbb6fa16485433f18e639d
Juan Jose <juanjoseblancodiaz1504@gmail.com>
2024-01-16 19:46:02 +0100

    Clase Racional

--- Ejercicios/Tema5/src/Ejercicios/Racional/AppRacional.java
+++ Ejercicios/Tema5/src/Ejercicios/Racional/AppRacional.java
@@ -1,8 +1,10 @@
 package Ejercicios.Racional;
-
+import Ejercicios.Racional.Racional;
 public class AppRacional {
+    
     public static void main(String[] args) {
-        Racional num1 = new Racional(5, 4);
-        System.out.println(num1.MCD(num1.getDenominador()));
+        Racional num1 = new Racional(4, 48);
+        Racional num2 = new Racional (7,8);
+        System.out.println(num1.suma(num2));
     }
 }
